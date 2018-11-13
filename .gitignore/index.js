503401e7be1ee0936c7d69220e0bc3296caa03ae
furious664654 <44979090+furious664654@users.noreply.github.com>
2018-11-13 16:32:55 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,6 +10,44 @@ bot.on('ready', function() {
 
 bot.login(process.env.TOKEN);
 
+bot.on('message', message => {
+    let command = message.content.split(" ")[0];
+    const args = message.content.slice(prefix.lenght).split(/ +/)
+    command = args.shift().toLowerCase();
+
+    if (command === "kick") {
+        let modRole = message.guild.roles.find("name", "STAFFPERM");
+        if (!message.member.roles.has(modRole.id)) {
+            return message.reply("Tu n'as pas la permission de faire cette commande !").catch(console.error);        
+        }
+        if(message.mentions.users.size === 0) {
+            return message.reply("Merci de mentionner l'utulisateur à expulser.").catch(console.error);
+        }
+        let kickMember = message.guild.member(message.mentions.users.first());
+        if(!kickMember) {
+            return message.reply("Cet utulisateur est introuvable ou impossible à expulser !")
+        }
+        if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
+            return message.reply("Je n'ai pas la permission KICK_MEMBERS pour faire ceci.").catch(console.error);
+        }
+        kickMember.kick().then(member => {
+            message.reply(`${member.user.username} a été expulsé avec succès !`).catch(console.error);
+            message.guild.channels.find("name", "general").send(`**${member.user.username} a été expulsé du discord par **${message.author.username}**!`)
+        }).catch(console.error)
+    }
+
+    if (command === "ban") {
+        let modRole = message.guild.roles.find("name", "STAFFPERM");
+        if(message.member.roles.has(modRole.id)) {
+            return message.reply("Tu n'as pas la permission de faire cette commande !").catch(console.error);
+        }
+        const member = message.mentions.members.first();
+        if (!member) return message.reply("Merci de mentionner l'utulisateur à bannir !");
+        member.ban().then(member => {
+            message.reply(`${member.user.username} a été banni avec succès !`).catch(console.error);
+            message.guild.channels.find("name", "general").send(`**${member.user.username} a été banni du discord par **${message.author.username}**`)
+        }).catch(console.error)
+}})
 
 bot.on('message',message => {
     if (message.content === prefix + "help"){
